878ece8a038421de4598a52d628173713482b466
Aymene Mansouri <aimenrma23@gmail.com>
2023-04-08 22:12:31 +0100

    only separet letters

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,67 @@
 <template>
   <div class="relative min-h-screen">
-    <div :class="{'opacity-100':thanks}" class="opacity-0 duration-1000 transition-opacity absolute left-1/2 transform -translate-x-1/2 text-center pt-20">
-      <span class="text-4xl">🙏</span> 
-      <br>
+    <div
+      :class="{ 'opacity-100': thanks }"
+      class="opacity-0 duration-1000 transition-opacity absolute left-1/2 transform -translate-x-1/2 text-center pt-20"
+    >
+      <span class="text-4xl">🙏</span>
+      <br />
       <span class="text-lg">Thank you for your contribution</span>
     </div>
-  <main>
-    <startPage v-if="!appStarted" @startApp = "startApp"/>
-    <drawingPage :time="time" v-if="appStarted" @closeDrawing = "closeDrawingFnc"/>
-  </main>
+    <main>
+      <startPage v-if="!appStarted" @startApp="startApp" />
+      <drawingPage
+        :time="time"
+        v-if="appStarted"
+        @closeDrawing="closeDrawingFnc"
+      />
+    </main>
 
-  <div class="absolute bottom-0 w-full">
-    <footer class="container">
-      <h3 class="text-xs pb-2 text-center">Made with ❤️ by Aimen</h3>
-    </footer>
+    <div class="absolute bottom-0 w-full">
+      <footer class="container">
+        <h3 class="text-xs pb-2 text-center">
+          Made with ❤️ by Aimen
+          <a
+            target="_blank"
+            class="underline font-semibold"
+            href="https://github.com/aimenmansouri/horof_collect-data-for-machine-learning"
+            >Github</a
+          >
+        </h3>
+      </footer>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import startPage from './components/startPage.vue'
-import drawingPage from './components/drawingPage.vue'
+import startPage from "./components/startPage.vue";
+import drawingPage from "./components/drawingPage.vue";
 
-
- export default {
-    name: 'App',
-    components :{
-      startPage,
-      drawingPage
+export default {
+  name: "App",
+  components: {
+    startPage,
+    drawingPage,
+  },
+  data() {
+    return {
+      appStarted: false,
+      time: 0,
+      thanks: false,
+    };
+  },
+  methods: {
+    startApp(time) {
+      this.time = time;
+      this.appStarted = true;
     },
-    data() {
-      return{
-        appStarted : false ,
-        time : 0 ,
-        thanks : false ,
-      }
+    closeDrawingFnc() {
+      this.appStarted = false;
+      this.thanks = true;
+      setTimeout(() => {
+        this.thanks = false;
+      }, 2500);
     },
-    methods : 
-    {
-      startApp(time){
-        this.time = time
-        this.appStarted = true;
-      },
-      closeDrawingFnc() {
-        this.appStarted = false; 
-        this.thanks = true
-        setTimeout(() => {
-            this.thanks = false
-        }, 2500)
-      }
-    },
-  }
+  },
+};
 </script>
-
